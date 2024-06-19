d4100b0ee6a967db8e21862a004e967a65bf2903
Connor Wiltshire <cwnz2004@gmail.com>
2024-06-19 15:09:58 +1200

    Reverted to working

--- tagChecker.ts
+++ tagChecker.ts
@@ -1,6 +1,6 @@
-// tagChecker.ts
 class TagChecker {
-    static checkTags(paragraph: string): string {   const tagPattern = /<\/?[A-Za-z][\w:-]*>/g;
+    static checkTags(paragraph: string): string {
+        const tagPattern = /<\/?[A-Z]>/g;
         const tags = paragraph.match(tagPattern);
         
         if (!tags) {
@@ -9,23 +9,28 @@ class TagChecker {
 
         const stack: string[] = [];
 
-
         for (let i = 0; i < tags.length; i++) {
-            const currentTag = tags[i];
-            let isClosingTag = false;
-
-            if (currentTag[1] === '/') {
-                isClosingTag = true;
-                const tagName = currentTag.slice(2, -1);
-                const expectedTag = `</${tagName}>`;
-                if (stack.length === 0 || stack[stack.length - 1] !== expectedTag) {
-                    return `Expected ${expectedTag} found ${currentTag}`;
-                }
-                stack.pop();
+            const tag = tags[i];
+            
+            if (tag[1] !== '/') {
+                stack.push(tag);
             } else {
-                const tagName = currentTag.slice(1, -1);
-                const openingTag = `<${tagName}>`;
-                stack.push(openingTag);
+                if (stack.length === 0) {
+                    return `Expected # found ${tag}`;
+                }
+
+                const lastTag = stack.pop();
+                
+                if (!lastTag) {
+                    return "Unexpected empty stack";
+                }
+
+                const tagName = lastTag.slice(1, -1); 
+                const expectedTag = `</${tagName}>`;
+
+                if (tag !== expectedTag) {
+                    return `Expected ${expectedTag} found ${tag}`;
+                }
             }
         }
 
@@ -34,7 +39,7 @@ class TagChecker {
             if (!lastTag) {
                 return "Unexpected empty stack";
             }
-            const tagName = lastTag.slice(1, -1);
+            const tagName = lastTag.slice(1, -1); 
             return `Expected </${tagName}> found #`;
         }
 
@@ -42,8 +47,7 @@ class TagChecker {
     }
 }
 
-
-// Input Example
+// Example usage
 const paragraphs = [
     "The following text <C><B>is centered and in boldface</B></C>",
     "<B>This <\\g>is <B>boldface</B> in <<*> a</B> <\\6> <<d> sentence",
